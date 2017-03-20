adf9b449e6067035ec98d3af8d22ba2d8963a4c5
naeohmi <naeohmi@gmail.com>
2017-03-19 23:03:56 -0700

    wdi fundamentals update

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -3,3 +3,13 @@ var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
+if (cardTwo === cardFour) {
+    alert("You found a match!");
+} else {
+    alert("Sorry, try again.");
+}
+if (cardOne === cardThree) {
+    alert("You found a match!");
+} else {
+    alert("Sorry, try again.");
+}
